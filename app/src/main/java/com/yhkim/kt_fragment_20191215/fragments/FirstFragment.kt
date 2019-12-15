742011768d5b80dc5e408ede695aba8d388cc910
kvoyant <kvoyant@gmail.com>
2019-12-15 15:50:07 +0900

    첫번째 프래그먼트 수정  - XML 따로 그려주기  - 이벤트 생성

--- app/src/main/java/com/yhkim/kt_fragment_20191215/fragments/FirstFragment.kt
+++ app/src/main/java/com/yhkim/kt_fragment_20191215/fragments/FirstFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.yhkim.kt_fragment_20191215.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment(){
 
@@ -16,5 +18,15 @@ class FirstFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        okBtn.setOnClickListener {
+            //내가 누구? mContext => activity
+            Toast.makeText(activity, "${nameEdt.text.toString()}", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
